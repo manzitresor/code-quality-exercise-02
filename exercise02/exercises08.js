a8c088aca48ebf8b0636dfdfca0a466fdd16d9a5
manzitresor <manzitre@gmail.com>
2024-08-22 17:13:22 +0200

    Add refactor exercise 8

--- exercise02/exercises08.js
+++ exercise02/exercises08.js
@@ -1,6 +1,6 @@
-function calculateDiscount(totalamount) {
-  if (totalamount > 100) {
-    return totalamount * 0.1;
+function calculateDiscount(amount) {
+  if (amount > 100) {
+    return amount * 0.1;
   } 
-  return totalamount * 0.05;
+  return amount * 0.05;
 }
