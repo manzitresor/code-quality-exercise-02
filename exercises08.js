730949257382fbc525d971887f45934cd3f0ab2d
manzitresor <manzitre@gmail.com>
2024-08-22 08:56:28 +0200

    Add exercises 08

--- exercises08.js
+++ exercises08.js
@@ -1,9 +1,6 @@
-function discount(totalamount) {
-  var disc;
+function FindDiscount(totalamount) {
   if (totalamount > 100) {
-    disc = totalamount * 0.1;
-  } else {
-    disc = totalamount * 0.05;
-  }
-  return disc;
+    return totalamount * 0.1;
+  } 
+  return totalamount * 0.05;
 }
