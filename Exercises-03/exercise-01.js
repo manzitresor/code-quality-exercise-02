e1a4f8d7832f2358c55bf74753f3198b62243ebe
manzitresor <manzitre@gmail.com>
2024-08-22 20:45:46 +0200

    Add function to check nested array

--- Exercises-03/exercise-01.js
+++ Exercises-03/exercise-01.js
@@ -1,5 +1,17 @@
-function checkNesting(arr) {
+function flattenArray(arr) {
+    let flattenedArray = [];
     if(!Array.isArray(arr)) return 0
+    for(let element of arr){
+        if(Array.isArray(element)){
+            const nestedArr = flattenArray(element);
+            for(let nestedElement of nestedArr) {
+                flattenedArray.push(nestedElement)
+            }
+        } else {
+            flattenedArray.push(element)
+        }
+    }
+    return flattenedArray ;
 }
-
-console.log(checkNesting(12))
+const arr = [1, [2, [3, [4,[2]]]]];
+console.log(flattenArray(arr))
