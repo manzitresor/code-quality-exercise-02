61a871786ee8ae1f73ebab0987b2f38a38ae525b
manzitresor <manzitre@gmail.com>
2024-08-22 08:46:37 +0200

    Add exercises 05

--- exercises05.js
+++ exercises05.js
@@ -1,9 +1,9 @@
-function calculateFactorial(n) {
-  if (n === 0 || n === 1) {
+function calculateFactorial(number) {
+  if (number === 0 || number === 1) {
     return 1;
   } else {
-    var factorial = 1;
-    for (var i = 2; i <= n; i++) {
+    let factorial = 1;
+    for (let i = 2; i <= number; i++) {
       factorial *= i;
     }
     return factorial;
