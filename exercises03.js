522c775ea69e2fc63f5690cf404e335b26abca9d
manzitresor <manzitre@gmail.com>
2024-08-22 08:39:34 +0200

    Add exercises 03

--- exercises03.js
+++ exercises03.js
@@ -1,6 +1,6 @@
 function reverseString(str) {
-  var reversed = "";
-  for (var i = str.length - 1; i >= 0; i--) {
+  let reversed = "";
+  for (let i = str.length - 1; i >= 0; i--) {
     reversed += str[i];
   }
   return reversed;
