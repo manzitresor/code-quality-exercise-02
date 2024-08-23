50e7863faf72fb46ab96bac1e6673ef45c88b515
manzitresor <manzitre@gmail.com>
2024-08-23 15:47:15 +0200

    Add Refactor excercise 2

--- Exercises-03/exercise-02.js
+++ Exercises-03/exercise-02.js
@@ -1,7 +1,7 @@
-function isPrime(num) {
-    if (num <= 1) return false;
-    for (let i = 2; i <= Math.sqrt(num); i++) {
-        if (num % i === 0) return false;
+function isPrime(number) {
+    if (number <= 1) return false;
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+        if (number % i === 0) return false;
     }
     return true;
 }
