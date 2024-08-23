7112d5812e08c36d120d621dc822b63815d0ba8f
manzitresor <manzitre@gmail.com>
2024-08-23 15:50:11 +0200

    Add refactor calculate Average function

--- exercise02/exercises01.js
+++ exercise02/exercises01.js
@@ -1,5 +1,4 @@
 function calculateAverage(numbers) {
     const sum = numbers.reduce((previousNumber, currentNumber) => previousNumber + currentNumber);
-    const count = numbers.length;
-    return sum / count;
+    return sum / numbers.length
   }
