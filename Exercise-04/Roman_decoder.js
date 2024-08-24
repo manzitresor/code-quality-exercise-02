e615e99eca29f559a83a8215ac92c465db7fa67f
manzitresor <manzitre@gmail.com>
2024-08-24 20:37:51 +0200

    Add Roman decoder refactor object

--- Exercise-04/Roman_decoder.js
+++ Exercise-04/Roman_decoder.js
@@ -1,15 +1,7 @@
 function romanDecoder (roman) {
     let total = 0;
     let prevValue = 0; 
-     const romanValues = {
-      'I': 1,
-      'V': 5,
-      'X': 10,
-      'L': 50,
-      'C': 100,
-      'D': 500,
-      'M': 1000
-    };
+     const romanValues = { 'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000 };
     
     for (let i = roman.length - 1; i >= 0; i--) {
       const currentValue = romanValues[roman[i]];
